6d8ae043eab80a500b71658db042e8da0ffd2cf0
Rylee <rylee@percy.io>
2021-04-19 14:10:55 -0700

    resetting master

--- README.md
+++ README.md
@@ -12,13 +12,10 @@ This app uses the demo [TodoMVC](https://github.com/tastejs/todomvc) app and tak
 To install dependencies, compile and run the Todo app:
 
 ```bash
-$ npm install
-$ open index.html
-```
-
-To run the PercyScript:
-```bash
-$ npm run snapshots
+$ npx percy exec -- node snapshots.js
 ```
 
 See `snapshots.js` file for the example script.
+
+export PERCY_API=http://localhost:9090/api/v1
+export PERCY_TOKEN=c69ed10cd0eb4669dd41f7cf06e29a95f2549d8f1c800737496807ffc002370e
--- snapshots.js
+++ snapshots.js
@@ -1,22 +1,32 @@
-const PercyScript = require('@percy/script');
-const httpServer = require('http-server');
+// const PercyScript = require('@percy/script');
+// const httpServer = require('http-server');
+//
+// const PORT = process.env.PORT_NUMBER || 8000;
+// const TEST_URL = `http://localhost:${PORT}`;
+//
+// // A script to navigate our app and take snapshots with Percy.
+// PercyScript.run(async (page, percySnapshot) => {
+//   let server = httpServer.createServer();
+//   server.listen(PORT);
+//
+//   console.log(`Server started at ${TEST_URL}`);
+//
+//   await page.goto(TEST_URL);
+//   await percySnapshot('TodoMVC home page');
+//
+//   // Enter a new to-do.
+//   await page.type('.new-todo', 'A really important todo');
+//   await page.keyboard.press('Enter');
+//   await percySnapshot('TodoMVC with a new todo', { widths: [768, 992, 1200] });
+//   server.close();
+// });
+//
 
-const PORT = process.env.PORT_NUMBER || 8000;
-const TEST_URL = `http://localhost:${PORT}`;
+const PercyScript = require("@percy/script");
 
-// A script to navigate our app and take snapshots with Percy.
 PercyScript.run(async (page, percySnapshot) => {
-  let server = httpServer.createServer();
-  server.listen(PORT);
-
-  console.log(`Server started at ${TEST_URL}`);
-
-  await page.goto(TEST_URL);
-  await percySnapshot('TodoMVC home page');
-
-  // Enter a new to-do.
-  await page.type('.new-todo', 'A really important todo');
-  await page.keyboard.press('Enter');
-  await percySnapshot('TodoMVC with a new todo', { widths: [768, 992, 1200] });
-  server.close();
+  await page.goto("https://localhost:4200/");
+  // ensure the page has loaded before capturing a snapshot
+  await page.waitFor(".hero-main-image");
+  await percySnapshot("homepage");
 });
